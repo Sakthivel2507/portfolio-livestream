1f2ab907d28e36d37b77cd7433caf28714587414
Sakthivel2507 <106909352+Sakthivel2507@users.noreply.github.com>
2022-06-09 03:55:51 +0530

    Update urls.py

--- portfoliowebsite/urls.py
+++ portfoliowebsite/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('', include('base.urls')),
     path('api/', include('base.api.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 
